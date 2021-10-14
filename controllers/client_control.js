b287ae997a95e5b3f719b9e1bebc3b2b4927a492
quocvd1511 <dinhquoc.van.2010@gmail.com>
2021-10-14 11:05:39 +0700

    cai dat lay thong tin sach tu databse de hien thi len trang home

--- controllers/client_control.js
+++ controllers/client_control.js
@@ -1,8 +1,19 @@
-class Client_Control{
-    main(req,res)
+const admin_login = require('../models/admin_account')
+const books=require('../models/books')
+const {multipleMongooseToObject} = require('../util/mongoose.js')
+const {mongooseToObject} = require('../util/mongoose.js')
+
+class Client_Control
+{
+    main(req,res,next)
     {
-            res.render('temp_client', {layout: 'client.handlebars'})
-          
+        books.find({})
+            .then(books => 
+                {
+                    books=books.map(course => course.toObject())
+                    res.render('home_client.handlebars',{layout:'client.handlebars', books})
+                })
+            .catch(next)
     }
 }
 
