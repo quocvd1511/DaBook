d71f30020f3fcf93bbbc00aa960d6a2cc80e0d8b
hongnguyen1508 <hongnguyenarmy@gmail.com>
2021-10-19 21:34:56 +0700

    tìm kiếm theo thể loại

--- controllers/client_control.js
+++ controllers/client_control.js
@@ -17,12 +17,44 @@ class Client_Control
             .catch(next)
     }
 
+    // Tìm kiếm theo tên sách, tác giả
     search(req,res,next)
     {   
          // lấy giá trị của key name trong query parameters gửi lên
-            books.find({ $or:[
-                {'tacgia':{$in: [req.query.name]}},
-                {'tensach':{$in: [req.query.name]}}
+            books.find({ $or :[
+                { 'tensach' : {'$regex' : req.query.name , '$options' : 'i'}},
+                { 'tacgia' :  {'$regex' : req.query.name , '$options' : 'i'}}
+            ]})
+            .then(books => 
+                {
+                    books=books.map(course => course.toObject())
+                    res.render('home_client.handlebars',{layout:'client.handlebars',books});
+                })
+            .catch(next)
+    }
+
+    // Tìm kiếm theo danh sách các thể loại
+    searchTL(req,res,next)
+    {   
+            books.find(
+                {'theloai':{'$regex' : req.params.value , '$options' : 'i'}}
+            )
+            .then(books => 
+                {
+                    books=books.map(course => course.toObject())
+                    res.render('home_client.handlebars',{layout:'client.handlebars',books});
+                })
+            .catch(next)
+    }
+    
+    // Tìm kiếm theo bộ lọc
+    search(req,res,next)
+    {   
+         // lấy giá trị của key name trong query parameters gửi lên
+            books.find({ $or :[
+                { 'giaban' : {'$regex' : req.query.name , '$options' : 'i'}},
+                { 'nxb' :  {'$regex' : req.query.name , '$options' : 'i'}}
+               // { 'ngonngu' :  {'$regex' : req.query.name , '$options' : 'i'}}
             ]})
             .then(books => 
                 {
--- routes/client_route.js
+++ routes/client_route.js
@@ -5,5 +5,6 @@ const client_Control = require('../controllers/client_control')
 
 route.get('/',client_Control.main)
 route.get('/search',client_Control.search)
+route.get('/:value',client_Control.searchTL)
 
 module.exports = route
