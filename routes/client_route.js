ff1eca1dc33ce07f822162a52b6c5b07ab8599e0
quocvd1511 <dinhquoc.van.2010@gmail.com>
2021-10-21 12:44:01 +0700

    fix loading css

--- routes/client_route.js
+++ routes/client_route.js
@@ -3,8 +3,9 @@ const route = express.Router()
 
 const client_Control = require('../controllers/client_control')
 
-route.get('/',client_Control.main)
-route.get('/search',client_Control.search)
-route.get('/:value',client_Control.searchTL)
+route.get('/search', client_Control.search)
+route.get('/theloai/:value', client_Control.searchTL)
+route.get('/', client_Control.main)
+
 
 module.exports = route
