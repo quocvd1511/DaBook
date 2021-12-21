7c71ca29cba116bde20723262e47aa780a56edfc
ThuyTrang2906 <19522384@gm.uit.edu.vn>
2021-12-21 22:33:22 +0700

    pull

--- models/client_account.js
+++ models/client_account.js
@@ -3,27 +3,30 @@ const Schema = mogoose.Schema;
 
 const client_account = new Schema({
     matk: String,
-    email: String,
+    hoten: String,
+    email: {type: String, default: ""},
     matkhau: String,
-    ngaytao: {type: Date, default: Date.now},
+    ngaytao: {type: Date, default: Date.now,  transform: v => v.getDate() + "/" + v.getMonth() + "/" + v.getFullYear()},
     diem: Number,
     tinhtrang: String,
-    danhsach_km: {
-      makm: String,
-    },
+    danhsach_km: Array,
     diachigh: {
       diachi: String,
     },
-    diachigoc: String,
+    diachigoc:{
+      type: Array ,  default: ["","","",""]
+    },
     gioitinh: String,
     sodt: String,
-    giohang: {
+    giohang: [{
+      masach: String,
       tensach: String,
       giaban: Number,
       hinhanh: String,
-      soluong: Number,
-    },
+      SoLuong: Number,
+    }],
     sl_giohang: Number,
+    diachi: {type: String, default: ""}
   });
   
 module.exports = mogoose.model('client_accounts',client_account)
--- models/donhang.js
+++ models/donhang.js
@@ -3,21 +3,17 @@ const Schema = mogoose.Schema;
 
 const donhang = new Schema({
     madh: String,
-    ngaylap: String,
+    ngaylap: {type: Date, default: Date.now, transform: v => v.getDate() + "/" + v.getMonth() + "/" + v.getFullYear()},
     matk: String,
     hinhthucthanhtoan: String,
     vanchuyen: String,
     makm: Array,
     tongtien: Number,
+    tiengiam: Number,
     thongtinnguoinhan: String,
     tinhtrangthanhtoan: String,
     tinhtrangdonhang: String,
-    ds_sach: {
-      tensach: String,
-      giaban: Number,
-      hinhanh: String,
-      soluong: Number,
-    },
+    ds_sach: Array
   });
   
 module.exports = mogoose.model('donhangs',donhang)
