ca040dc73d5b5a4d09b639fa98e8aecbb2fe28b3
ThuyTrang2906 <19522384@gm.uit.edu.vn>
2021-12-24 10:47:27 +0700

    fix payment

--- id_facebook.js
+++ id_facebook.js
@@ -1,6 +1,6 @@
 const id=
 {
-    clientid: "1585513025115259",
-    secretid: "6eba2961329ccbcdad2a580bcc184eab",
+    clientid: "751828025807001",
+    secretid: "c83b94a3bba48d441d700ecd5c6766c6",
 }
 module.exports = id
